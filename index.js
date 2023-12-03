1e4a6dcc63d2915096f522a486f8c5f81dfe46e5
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-03 14:35:49 -0500

    added additional user option/prompt field

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer')
-const connection = require('./server')
-const { addEmployee, addDepartment, addTitle, addSalary, updEmployeeName, updDepartment, updTitle, updSalary, removeRecord} = require ('./functions')
+//const connection = require('./server')
+const { addEmployee, addDepartment, addTitle, addSalary, updEmployeeName, updDepartment, updTitle, updSalary, removeRecord } = require ('./functions')
 const userOptions = ['Add new employee','Add Department','Add Title','Add Salary','Update Employee Name','Update Department','Update Title','Update Salary', 'Remove Record']
 
 //User Prompts
-inquirer.prompt({type: 'list', name: 'Action', messsage: 'Choose an Action', choices: userOptions})
+inquirer.prompt({type: 'list', name: 'action', messsage: 'Choose an Action', choices: userOptions})
     .then(((answer) => {switch (answer.action) {
       case 'Add new employee': addEmployee(); break;
       case 'Add department': addDepartment(); break;
@@ -15,4 +15,5 @@ inquirer.prompt({type: 'list', name: 'Action', messsage: 'Choose an Action', cho
       case 'Update title': updTitle(); break;
       case 'Update salary': updSalary(); break;
       case 'Remove Record': removeRecord(); break;
+      case 'View Employees': viewEmployees(); break;
     }}));
