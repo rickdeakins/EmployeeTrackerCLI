051240757cc6f681046c1906299227f258acafd2
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-03 05:50:20 -0500

    Functions file established to house logic for each user choice. Logic to be added in a future commit.

---
+++ functions.js
@@ -0,0 +1,12 @@
+const connection = require('./server')
+
+function addEmployee() {}
+function addDepartment() {}
+function addTitle() {}
+function addSalary() {}
+function updEmployeeName() {}
+function updDepartment() {}
+function updTitle() {}
+function removeRecord() {}
+
+module.exports = { addEmployee, addDepartment, addTitle, addSalary, updEmployeeName, updDepartment, updTitle, removeRecord};
