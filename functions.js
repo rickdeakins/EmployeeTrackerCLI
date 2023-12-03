43313998faf83cb015535fb915be119170ad6be8
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-03 14:36:24 -0500

    added additional function for new field

--- functions.js
+++ functions.js
@@ -1,12 +1,27 @@
 const connection = require('./server')
 
-function addEmployee() {}
-function addDepartment() {}
-function addTitle() {}
-function addSalary() {}
-function updEmployeeName() {}
-function updDepartment() {}
-function updTitle() {}
-function removeRecord() {}
+function addEmployee() {
+    //user should be prompted for all fields of data
+}
 
-module.exports = { addEmployee, addDepartment, addTitle, addSalary, updEmployeeName, updDepartment, updTitle, removeRecord};
+function addDepartment() {}
+
+function addTitle() {}
+
+function addSalary() {}
+
+function updEmployeeName() {}
+
+function updDepartment() {}
+
+function updTitle() {}
+
+function removeRecord() {
+    // Prompt user for employee ID then SQL DELETE command to remove the employee from the database 
+}
+
+function viewEmployees() {
+    // Prompt user for employee ID then SQL DELETE command to remove the employee from the database 
+}
+
+module.exports = { addEmployee, addDepartment, addTitle, addSalary, updEmployeeName, updDepartment, updTitle, removeRecord, viewEmployees };
