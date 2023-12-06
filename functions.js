a743e0ee08e34aec459e3164e9c2be02e364fb16
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-05 22:44:52 -0500

    continuing the functions for inquirer npm

--- functions.js
+++ functions.js
@@ -1,27 +1,52 @@
-const connection = require('./server')
+const connection = require("./server");
 
+// Prompt user for all fields of data
 function addEmployee() {
-    //user should be prompted for all fields of data
+  {
+    const sqlQuery = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+    connection.query(
+      sqlQuery,
+      [firstName, lastName, roleId, managerId],
+      (err, res) => {
+        if (err) throw err;
+        console.log("Employee added successfully!");
+      }
+    );
+  }
 }
 
-function addDepartment() {}
+function updEmployeeName() {
+  {
+    const sqlQuery = `UPDATE users SET first_name = '${newFirstName}', last_name = '${newLastName}'`;
+    connection.query(sqlQuery, [newFirstName, newLastName], (err, res) => {
+      if (err) throw err;
+      console.log("Employee added successfully!");
+    });
+  }
+}
 
-function addTitle() {}
-
-function addSalary() {}
-
-function updEmployeeName() {}
+//email = '${newEmail}' WHERE id = ${userId}`;
 
 function updDepartment() {}
 
 function updTitle() {}
 
 function removeRecord() {
-    // Prompt user for employee ID then SQL DELETE command to remove the employee from the database 
+  // Prompt user for employee ID then SQL DELETE command to remove the employee from the database
 }
 
 function viewEmployees() {
-    // Prompt user for employee ID then SQL DELETE command to remove the employee from the database 
+  // Prompt user for employee ID then SQL DELETE command to remove the employee from the database
 }
 
-module.exports = { addEmployee, addDepartment, addTitle, addSalary, updEmployeeName, updDepartment, updTitle, removeRecord, viewEmployees };
+module.exports = {
+  addEmployee,
+  addDepartment,
+  addTitle,
+  addSalary,
+  updEmployeeName,
+  updDepartment,
+  updTitle,
+  removeRecord,
+  viewEmployees,
+};
