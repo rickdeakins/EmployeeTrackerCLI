fbda09fd91370b383c53131dfcba5c76c67896e7
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-07 22:16:04 -0500

    revised queries

--- db/queries.sql
+++ db/queries.sql
@@ -1,12 +1,12 @@
 -- View Employees 
 SELECT e.id, e.first_name, e.last_name, r.title, d.name, e.manager_id
-FROM employee e
-LEFT JOIN role r ON e.role_id = r.id
-LEFT JOIN department d ON r.department_id = d.id;
+FROM employee AS e
+LEFT JOIN role AS r ON e.role_id = r.id
+LEFT JOIN department AS d ON r.department_id = d.id;
 
 -- Modify Department Name
 USE employeetracker_db;
-ALTER TABLE department ADD ${departmentName} VARCHAR(30);
+UPDATE department SET name = ${departmentName} WHERE id = ("id #");
 
 -- Add Employee
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?);
