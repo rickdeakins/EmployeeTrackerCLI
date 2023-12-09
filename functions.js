02f3ab24cd2d8d254281ccc3366d6dd2c9ee33a3
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-09 15:17:07 -0500

    adjustments to function names and sql queries to link to index file

--- functions.js
+++ functions.js
@@ -1,38 +1,24 @@
 const connection = require("./server");
 
+//Function to display all employees  
+function viewEmployees() {
+  const sqlQuery = `USE employeetracker_db; SELECT e.id AS employee_id, e.first_name, e.last_name, r.title AS role_title, r.salary, d.department_name, e.manager_id FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id;`;
+  connection.query(sqlQuery, (err, res) => {if (err) {console.error("Error viewing employees:", err);throw err;}
+    console.log("Employees:"); console.table(res);})}
+
 // Prompt user for all fields of data
 function addEmployee() {
-    const sqlQuery = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
-    connection.query(sqlQuery,[firstName, lastName, roleId, managerId],(err, res) => {
+    const sqlQuery = `INSERT INTO employee (first_name, last_name, role_id, name, manager_id) VALUES (?, ?, ?, ?)`;
+    connection.query(sqlQuery,[firstName, lastName, roleId, name, managerId],(err, res) => {
         if (err) throw err;console.log("Employee added successfully!");});}
 
-function updEmployeeName() {
-    const sqlQuery = `UPDATE employee SET first_name = '${newFirstName}', last_name = '${newLastName}'`;
-    connection.query(sqlQuery, [newFirstName, newLastName], (err, res) => {
-      if (err) throw err;console.log("Employee name updated successfully!");});}
+function editEmployee() {}
+function removeEmployee() {}
 
-//email = '${newEmail}' WHERE id = ${userId}`;
-
-function updDepartment() {}
-
-function updTitle() {}
-
-function removeRecord() {
-  // Prompt user for employee ID then SQL DELETE command to remove the employee from the database
-}
-
-function viewEmployees() {
-  // Prompt user for employee ID then SQL DELETE command to remove the employee from the database
-}
 
 module.exports = {
-  addEmployee,
-  addDepartment,
-  addTitle,
-  addSalary,
-  updEmployeeName,
-  updDepartment,
-  updTitle,
-  removeRecord,
   viewEmployees,
+  addEmployee,
+  editEmployee,
+  removeEmployee
 };
