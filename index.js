f61b1a5afd980b04e020a5349c4728bebc02481a
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-09 15:16:28 -0500

    restructured prompting inquiry

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const inquirer = require('inquirer')
-//const connection = require('./server')
-const { addEmployee, addDepartment, addTitle, addSalary, updEmployeeName, updDepartment, updTitle, updSalary, removeRecord } = require ('./functions')
-const userOptions = ['Add new employee','Add Department','Add Title','Add Salary','Update Employee Name','Update Department','Update Title','Update Salary', 'Remove Record']
+const connection = require('./server')
+const { viewEmployees, addEmployee, editEmployee, removeEmployee } = require ('./functions')
+//const userOptions = ['Add new employee','Add Department','Add Title','Add Salary','Update Employee Name','Update Department','Update Title','Update Salary', 'Remove Record']
 
-//User Prompts
-inquirer.prompt({type: 'list', name: 'action', messsage: 'Choose an Action', choices: userOptions})
-    .then(((answer) => {switch (answer.action) {
-      case 'Add new employee': addEmployee(); break;
-      case 'Update Employee Name': updEmployeeName(); break;
-      case 'Update Department': updDepartment(); break;
-      case 'Update title': updTitle(); break;
-      case 'Update salary': updSalary(); break;
-      case 'Remove Record': removeRecord(); break;
-      case 'View Employees': viewEmployees(); break;
-    }}));
+async function main() {
+  while (true) {
+    const action = await showMainMenu();
+    switch (action) {
+      case 'View Employees': await viewEmployees(); break;
+      case 'Add Employee': await addEmployee(); break;
+      case 'Edit Employee': await editEmployee(); break;
+      case 'Remove Employee': await removeEmployee(); break;
+      case 'Exit':console.log('Exiting application. Goodbye!'); 
+      process.exit(0); default: console.log('Invalid choice. Please try again.');break;
+    }}}
 
+async function showMainMenu() {
+  const { action } = await inquirer.prompt({
+    type: 'list',
+    name: 'action',
+    message: 'Choose an action:',
+    choices: ['View Employees','Add New Employee','Edit Existing Employee', 'Remove Employee', 'Exit'],
+  });return action;}
+
+
+
+
+main();
