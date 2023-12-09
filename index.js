0683a3c75bf8ce3f64e61cc44f5d2fd598452cab
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-09 15:53:10 -0500

    inquirer prompts for new employee

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ async function main() {
     const action = await showMainMenu();
     switch (action) {
       case 'View Employees': await viewEmployees(); break;
-      case 'Add Employee': await addEmployee(); break;
+      case 'Add Employee': await handleAddEmployee(); break;
       case 'Edit Employee': await editEmployee(); break;
       case 'Remove Employee': await removeEmployee(); break;
       case 'Exit':console.log('Exiting application. Goodbye!'); 
@@ -21,5 +21,46 @@ async function showMainMenu() {
     message: 'Choose an action:',
     choices: ['View Employees','Add New Employee','Edit Existing Employee', 'Remove Employee', 'Exit'],
   });return action;}
+
+  async function handleAddEmployee() {
+    const employeeDetails = await addEmployeeDetails();
+    await addEmployee(employeeDetails.firstName, employeeDetails.lastName, employeeDetails.roleTitle, employeeDetails.salary, employeeDetails.departmentName, employeeDetails.managerId);}
+  
+  async function addEmployeeDetails() {
+    const questions = [
+      {
+        type: 'input',
+        name: 'firstName',
+        message: 'Enter the first name:',
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: 'Enter the last name:',
+      },
+      {
+        type: 'input',
+        name: 'roleTitle',
+        message: 'Enter the role ID:',
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'Enter the salary amount:',
+      },
+      {
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter the department name:',
+      },
+      {
+        type: 'input',
+        name: 'managerId',
+        message: 'Enter the manager ID:',
+      },
+    ];
+  
+    return inquirer.prompt(questions);
+  }
 
 main();
