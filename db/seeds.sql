c7e2d0785b278e830d8866d024714d3d717f41f7
rickdeakins <rickdeakinsjr@gmail.com>
2023-12-11 23:26:16 -0500

    seeded data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,37 @@
-INSERT INTO department (department_name) VALUES ("Sales");
-INSERT INTO department (department_name) VALUES ("IT");
 INSERT INTO department (department_name) VALUES ("Management");
+INSERT INTO department (department_name) VALUES ("IT");
+INSERT INTO department (department_name) VALUES ("Mergers");
+INSERT INTO department (department_name) VALUES ("Sales");
+INSERT INTO department (department_name) VALUES ("Operations");
+INSERT INTO department (department_name) VALUES ("Marketing");
 
-INSERT INTO role (title, salary, department_id) VALUES ("Sales Associate", 50000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("System Administrator", 82500, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Operations Manager", 101000, 3);
+
+
+
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Associate", 58257, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Sales Manager", 88248, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Intern", 41250, 4);
+INSERT INTO role (title, salary, department_id) VALUES ("Intern", 41250, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Intern", 41250, 5);
+INSERT INTO role (title, salary, department_id) VALUES ("Intern", 41250, 6);
+INSERT INTO role (title, salary, department_id) VALUES ("Intern", 52340, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 84543, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("System Administrator", 93200, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Data Analyst", 72340, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Data Analyst", 74300, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Data Analyst", 73400, 3);
+INSERT INTO role (title, salary, department_id) VALUES ("Data Analyst", 71029, 5);
+INSERT INTO role (title, salary, department_id) VALUES ("Marketing Manager", 50297, 6); 
+INSERT INTO role (title, salary, department_id) VALUES ("Operations Analyst", 44500, 5);
+
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Smith", 1, 2);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("David", "James", 2, 1);
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Melissa", "Jones", 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Maria", "Gonzales", 1, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Robert", "James", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Harold", "Savauge", 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Johnathan", "Franklin", 4,2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Marcus", "White", 5, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Franlin", "Jones", 6, 3);
